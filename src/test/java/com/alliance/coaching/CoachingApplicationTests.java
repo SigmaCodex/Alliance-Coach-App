7b45e8b0358d86bbe49ae052057fc526680d4398
Rieugene Basilisco <generieu17@gmail.com>
2022-03-26 12:23:17 +0800

    new: re-initialized commit

---
+++ src/test/java/com/alliance/coaching/CoachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alliance.coaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
