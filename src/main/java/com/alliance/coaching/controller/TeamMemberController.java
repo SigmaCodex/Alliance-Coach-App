56b4b5614762408755095fa049a8c2273e1f6030
Rieugene Basilisco <generieu17@gmail.com>
2022-03-28 16:00:10 +0800

    new: login and register function

--- src/main/java/com/alliance/coaching/controller/TeamMemberController.java
+++ src/main/java/com/alliance/coaching/controller/TeamMemberController.java
@@ -1,9 +1,11 @@
 package com.alliance.coaching.controller;
 
+import com.alliance.coaching.entity.TeamMember;
 import com.alliance.coaching.service.TeamMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Rieugene Basilisco (generieu17@gmail.com)
@@ -19,5 +21,30 @@ public class TeamMemberController {
     private final TeamMemberService teamMemberService;
 
     // TODO: 3/26/2022 CRUD for team member
+
+    @PostMapping("/")
+    public ModelAndView registerEmployee(@ModelAttribute TeamMember teamMember) {
+        ModelAndView modelAndView = new ModelAndView();
+        TeamMember savedTeamMember = teamMemberService.create(teamMember);
+        modelAndView.setViewName("home");
+        modelAndView.addObject("team_member", savedTeamMember);
+        return modelAndView;
+    }
+
+    // TODO: 3/28/2022 login team member
+    @GetMapping("/login")
+    public String loginTeamMember(@RequestParam String username,
+                                  @RequestParam String password) {
+        TeamMember teamMember = teamMemberService.loginEmployee(username, password);
+        // TODO: 3/28/2022 HR portal
+        if (teamMember.getEmployeeType().equals("HR")) {
+            return "hr_portal";
+        }
+        // TODO: 3/28/2022 Coach portal
+        if (teamMember.getEmployeeType().equals("Coach")) {
+            return "coach_portal";
+        }
+        return "index";
+    }
 
 }
